1be176cfdfd2dbd19d10920d8287e264d12146cd
VickyPhu <victoriaphu@gmail.com>
2025-01-15 15:49:03 +0100

    set up attributes and method
    Co-authored-by: Axziomatic <Axziomatic@users.noreply.github.com>
    Co-authored-by: borghanna <borghanna@users.noreply.github.com>
    Co-authored-by: Johan Victorin <johan.victorin@medieinstitutet.se>
    Co-authored-by: Krnsand <Krnsand@users.noreply.github.com>

---
+++ src/button.ts
@@ -0,0 +1,10 @@
+class Button {
+    private text: string;
+    private color: string;
+    private width: number;
+    private height: number;
+    private posX: number;
+    private posY: number;
+
+    public draw();
+}
