710ca0175853552f9c84a5fe8f56d886e729f4f5
VickyPhu <victoriaphu@gmail.com>
2025-01-15 21:36:18 +0100

    removed background

--- src/startmenu.ts
+++ src/startmenu.ts
@@ -2,12 +2,10 @@ import { Button } from './button.js';
 import { Screen } from './screen.js';
 
 export class StartMenu implements Screen {
-    private background: string;
     private startButton: Button;
     private howToPlayButton: Button;
 
     constructor() {
-        this.background = '#F0DEB5';
         this.startButton = new Button('START', '#F96B6B', 100, 50, 750, 280);
         this.howToPlayButton = new Button('HOW TO PLAY', '#F0AB63', 100, 50, 750, 400);
     }
