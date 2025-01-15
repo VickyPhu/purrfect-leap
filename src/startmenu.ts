7b94a2b29c65d0a11f82dc6a02ca7cb7672d7ffc
VickyPhu <victoriaphu@gmail.com>
2025-01-15 21:21:42 +0100

    set up constructor with background, and Buttons passed down from class Button

--- src/startmenu.ts
+++ src/startmenu.ts
@@ -6,6 +6,12 @@ export class StartMenu implements Screen {
     private startButton: Button;
     private howToPlayButton: Button;
 
+    constructor() {
+        this.background = '#F0DEB5';
+        this.startButton = new Button('START', '#F96B6B', 100, 50, 750, 280);
+        this.howToPlayButton = new Button('HOW TO PLAY', '#F0AB63', 100, 50, 750, 400);
+    }
+
     private drawButtons() {}
     private drawTitle() {}
 
