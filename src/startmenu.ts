298420cf3e0765927073139c126c45161c262b53
VickyPhu <victoriaphu@gmail.com>
2025-01-15 21:10:27 +0100

    implements interface screen with its methods

--- src/startmenu.ts
+++ src/startmenu.ts
@@ -1,11 +1,15 @@
 import { Button } from './button.js';
 import { Screen } from './screen.js';
 
-export class StartMenu extends Screen {
-
+export class StartMenu implements Screen {
+    private background: string;
     private startButton: Button;
     private howToPlayButton: Button;
 
     private drawButtons() {}
     private drawTitle() {}
+
+    public update() {}
+
+    public draw() {}
 }
