4763fb16c00b7cb55d0523ee3795e13e01b9939f
Krnsand <krnsandin@gmail.com>
2025-01-16 16:14:42 +0100

    Start code for playerSelect

--- src/playerselect.ts
+++ src/playerselect.ts
@@ -1,8 +1,32 @@
-class PlayerSelect extends Screen {
+class PlayerSelect implements IScreen {
   private playerSelectButton: Button;
-  private gameStartButton: Button;
+  private startButton: Button;
 
-  private drawImage() {}
-  private drawPlayerSelectButton() {}
-  private drawStartButton() {}
+  constructor() {
+    this.playerSelectButton = new Button('1 PLAYER', '#F96B6B', 350, 100, 500, 250);
+    this.playerSelectButton = new Button('2 PLAYER', '#C2E1B5', 350, 100, 500, 250);
+    this.playerSelectButton = new Button('3 PLAYER', '#F0AB63', 350, 100, 500, 250);
+    this.playerSelectButton = new Button('4 PLAYER', '#CBA3D2', 350, 100, 500, 250);
+    this.startButton = new Button('START GAME', '#F96B6B', 350, 100, 500, 380);
+  }
+
+  private drawImage() {
+
+  }
+  private drawPlayerSelectButton() {
+    this.playerSelectButton.draw();
+  }
+  private drawStartButton() {
+    this.startButton.draw();
+  }
+
+  public update() {}
+
+    public draw() {
+       fill('#F0DEB5')
+        noStroke();
+        rect(390, 190, 580, 350, 50);
+        this.drawTitle();
+        this.drawButtons();
+    }
 }
