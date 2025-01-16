11cd9ea64e2f3dbb9c28b6f6c0b1d9372bea27d4
Krnsand <krnsandin@gmail.com>
2025-01-16 16:24:04 +0100

    Add buttons to playerSelect

--- src/playerselect.ts
+++ src/playerselect.ts
@@ -1,6 +1,7 @@
 class PlayerSelect implements IScreen {
   private playerSelectButton: Button;
   private startButton: Button;
+  // gameStartButton?
 
   constructor() {
     this.playerSelectButton = new Button('1 PLAYER', '#F96B6B', 350, 100, 500, 250);
@@ -8,16 +9,20 @@ class PlayerSelect implements IScreen {
     this.playerSelectButton = new Button('3 PLAYER', '#F0AB63', 350, 100, 500, 250);
     this.playerSelectButton = new Button('4 PLAYER', '#CBA3D2', 350, 100, 500, 250);
     this.startButton = new Button('START GAME', '#F96B6B', 350, 100, 500, 380);
+    // gameStartButton?
   }
 
   private drawImage() {
+ const img = loadImage('.../assets/images/cats/Purr4.jpg');
+ image(img, 0, 0, width, height); 
 
   }
   private drawPlayerSelectButton() {
-    this.playerSelectButton.draw();
+    this.playerSelectButton.draw()
   }
   private drawStartButton() {
-    this.startButton.draw();
+    this.startButton.draw()
+    // gameStartButton?
   }
 
   public update() {}
