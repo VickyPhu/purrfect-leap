1417449f71a65ef5c1f019d37b83065bba4a2894
VickyPhu <victoriaphu@gmail.com>
2025-01-17 14:15:17 +0100

    changed order x, y, width, height

--- src/button.ts
+++ src/button.ts
@@ -1,25 +1,25 @@
 class Button {
   private text: string;
   private color: string;
-  private width: number;
-  private height: number;
   private posX: number;
   private posY: number;
+  private width: number;
+  private height: number;
 
   constructor(
     text: string,
     color: string,
+    posX: number,
+    posY: number,
     width: number,
     height: number,
-    posX: number,
-    posY: number
   ) {
     this.text = text;
     this.color = color;
-    this.width = width;
-    this.height = height;
     this.posX = posX;
     this.posY = posY;
+    this.width = width;
+    this.height = height;
   }
 
   public draw() {
@@ -31,6 +31,7 @@ class Button {
     //Text in button
     fill("#000");
     textFont("Fredoka", 45);
+    textStyle(BOLD);
     textAlign("center","center");
     text(this.text, this.posX + this.width / 2, this.posY + this.height / 2);
   }
