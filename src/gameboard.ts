a75c242b9b92b6b90037af1eeb3d1b87c5b83e7a
VickyPhu <victoriaphu@gmail.com>
2025-01-22 11:14:04 +0100

    added timer

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -2,6 +2,13 @@ class GameBoard implements IScreen {
   public backgroundImage: string;
   private backgroundMusic: string;
   private gameEntity: GameEntity[];
+  private startTime: number;
+  private elapsedTime: number;
+
+  constructor() {
+    this.startTime = millis();
+    this.elapsedTime = 0; 
+  }
 
   private spawnPlatform() {}
 
@@ -9,11 +16,22 @@ class GameBoard implements IScreen {
 
   private spawnPlayer() {}
 
-  private updateTime() {}
+  private updateTime() {
+    this.elapsedTime = (millis)() - this.startTime / 1000;
+  }
 
   private detectHit() {}
 
-  public update() {}
+  public update() {
+    this.updateTime();
+  }
 
-  public draw() {}
+  public draw() {
+    push();
+    fill('#000');
+    textAlign(RIGHT, CENTER);
+    textFont('Fredoka', 30);
+    text(`Time: ${nf(this.elapsedTime, 1, 1)}sec`, width - 10, 30);
+    pop();
+  }
 }
