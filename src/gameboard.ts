872414bede675fe8d4613b55465ada0896625562
VickyPhu <victoriaphu@gmail.com>
2025-01-22 11:53:51 +0100

    added countdown before game starts

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -4,10 +4,15 @@ class GameBoard implements IScreen {
   private gameEntity: GameEntity[];
   private startTime: number;
   private elapsedTime: number;
+  private countdownTime: number;
+  private countdownEnd: boolean;
+  private countdownValue: number;
 
   constructor() {
-    this.startTime = millis();
     this.elapsedTime = 0; 
+    this.countdownTime = millis();
+    this.countdownValue = 3;
+    this.countdownEnd = false;
   }
 
   private spawnPlatform() {}
@@ -16,22 +21,50 @@ class GameBoard implements IScreen {
 
   private spawnPlayer() {}
 
-  private updateTime() {
-    this.elapsedTime = (millis)() - this.startTime / 1000;
-  }
-
   private detectHit() {}
 
+  private updateCountdown() {
+    // count passed time since countdown and update the countdown value
+    const timePassed = Math.floor((millis() - this.countdownTime) / 1000);
+    this.countdownValue = 3 - timePassed;
+
+    // when countdown is finished start timer for the game
+    if (this.countdownValue <= 0) {
+      this.countdownEnd = true;
+      this.startTime = millis();
+    }
+  }
+
+  private updateTime() {
+    // update the games time
+    if (this.countdownEnd) {
+      this.elapsedTime = (millis() - this.startTime) / 1000;
+    }
+  }
+
   public update() {
-    this.updateTime();
+    if (!this.countdownEnd) {
+      this.updateCountdown();
+    } else {
+      this.updateTime();
+    }
   }
 
   public draw() {
     push();
     fill('#000');
-    textAlign(RIGHT, CENTER);
-    textFont('Fredoka', 30);
-    text(`Time: ${nf(this.elapsedTime, 1, 1)}sec`, width - 10, 30);
+    textAlign(CENTER, CENTER);
+    textFont('Fredoka', 300);
+
+    if (!this.countdownEnd) {
+      text(this.countdownValue, width * 0.5, height * 0.5);
     pop();
+    } else {
+      push();
+      textAlign(RIGHT, CENTER);
+      textSize(50);
+      text(`Time: ${nf(this.elapsedTime, 1, 1)}sec`, width - 10, 30);
+      pop();
+    }
   }
 }
