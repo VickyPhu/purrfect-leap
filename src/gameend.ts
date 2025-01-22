636455644bae2c6e07738e047f60733c21d393d3
Jim Bothén <jim.bothen@gmail.com>
2025-01-22 10:28:59 +0100

    Completed functionality to navigate between screens.

--- src/gameend.ts
+++ src/gameend.ts
@@ -2,8 +2,11 @@ class GameEnd implements IScreen {
   private buttons: Button[];
   private activeButtonIndex: number;
   private deadCat: p5.Image;
+  private enterKeyHasBeenReleased: boolean;
+
 
   constructor() {
+    this.enterKeyHasBeenReleased = false;
     this.buttons = [
       new Button("PLAY AGAIN", "#c2e1b5", 450, 500, 300, 100, 0),
       new Button("MAIN MENU", "#f0ab63", 950, 500, 300, 100, 1),
@@ -13,16 +16,22 @@ class GameEnd implements IScreen {
   }
 
   public update() {
-    if (keyIsDown(RIGHT_ARROW)) {
-      this.activeButtonIndex = +1;
-    } else if (keyIsDown(LEFT_ARROW)) {
-      this.activeButtonIndex = 0;
-    } else if (keyIsDown(ENTER)) {
+    if (keyIsDown(ENTER) && this.enterKeyHasBeenReleased) {
       if (this.activeButtonIndex === 0) {
         game.changeScreen("GameBoard");
       } else if (this.activeButtonIndex === 1) {
         game.changeScreen("StartMenu");
       }
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.activeButtonIndex = +1;
+    } else if (keyIsDown(LEFT_ARROW)) {
+      this.activeButtonIndex = 0;
+    }
+
+    if (!keyIsDown(ENTER)) {
+      this.enterKeyHasBeenReleased = true;
     }
   }
 
