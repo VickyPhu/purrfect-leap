c46173c18e3def0d57f0eda78b5521d29acf7f43
VickyPhu <victoriaphu@gmail.com>
2025-01-22 23:12:53 +0100

    removed timer and countdown from gameBoard and added it in class Time

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -4,21 +4,14 @@ class GameBoard implements IScreen {
   private players: Player[];
   private platforms: Platform[];
   private backgroundMusic: string;
-  private startTime: number;
-  private elapsedTime: number;
-  private countdownTime: number;
-  private countdownEnd: boolean;
-  private countdownValue: number;
+  private time: Time;
 
   constructor(playerImages: p5.Image[]) {
     this.playerImages = playerImages;
     this.players = [];
     this.platforms = [];
     this.loadImages();
-    this.elapsedTime = 0;
-    this.countdownTime = millis();
-    this.countdownValue = 3;
-    this.countdownEnd = false;
+    this.time = new Time();
   }
 
   private loadImages() {
@@ -27,31 +20,12 @@ class GameBoard implements IScreen {
     );
   }
 
-  private updateCountdown() {
-    // count passed time since countdown and update the countdown value
-    const timePassed = Math.floor((millis() - this.countdownTime) / 1000);
-    this.countdownValue = 3 - timePassed;
-
-    // when countdown is finished start timer for the game
-    if (this.countdownValue <= 0) {
-      this.countdownEnd = true;
-      this.startTime = millis();
-    }
-  }
-
-  private updateTime() {
-    // update the games time
-    if (this.countdownEnd) {
-      this.elapsedTime = (millis() - this.startTime) / 1000;
-    }
-  }
-
   private detectHit() {}
-  
+
   private drawBackground() {
     image(this.backgroundImage, 0, 0, 1400, 700);
   }
-  
+
   private spawnPlatform() {}
 
   private spawnPlayer() {
@@ -62,12 +36,12 @@ class GameBoard implements IScreen {
     this.spawnPlayer();
   }
 
-
   public update() {
-    if (!this.countdownEnd) {
-      this.updateCountdown();
+    // Updates the countdown and when countdown is over runs the else condition (starts timer)
+    if (!this.time.countdownEnd) {
+      this.time.updateCountdown();
     } else {
-      this.updateTime();
+      this.time.updateTimer();
     }
 
     this.players.forEach((player) => player.update());
@@ -76,21 +50,7 @@ class GameBoard implements IScreen {
   public draw() {
     this.drawBackground();
     this.players.forEach((player) => player.renderPlayer());
-    
-    push();
-    fill("#000");
-    textAlign(CENTER, CENTER);
-    textFont("Fredoka", 300);
-
-    if (!this.countdownEnd) {
-      text(this.countdownValue, width * 0.5, height * 0.5);
-      pop();
-    } else {
-      push();
-      textAlign(LEFT, CENTER);
-      textSize(50);
-      text(`Time: ${nf(this.elapsedTime, 1, 1)}sec`, 0, 30);
-      pop();
-    }
+    this.time.drawCountdown();
+    this.time.drawTimer();
   }
 }
